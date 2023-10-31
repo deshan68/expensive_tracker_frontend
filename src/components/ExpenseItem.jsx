3f27be2319c4fa65d606e2b76f3319f68aaa4703
deshan68 <arundeshan@gmail.com>
2023-10-31 12:20:18 +0530

    add tailwind

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseItem = ({ title, amount, date }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>{amount}</div>
+      <div>{date.toISOString()}</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
